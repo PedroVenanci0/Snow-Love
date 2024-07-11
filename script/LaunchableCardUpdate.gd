d90e1f09c4a4cc35cd4498e49cab56fd54ebf6b3
ArthurV10 <arthurvms10@gmail.com>
2024-07-11 08:14:31 -0300

    blabla

--- script/LaunchableCardUpdate.gd
+++ script/LaunchableCardUpdate.gd
@@ -1,20 +1,29 @@
 extends RigidBody2D
 
 var dragging 
-var dragStart = Vector2.ZERO
+var dragStart = Vector2.ZERO;
+var initialPos = Vector2.ZERO;
+
+func _ready():
+	initialPos = position;
 
 func _process(delta):
 	if dragging == false:
 		pass
 		#global_position = Vector2(0.0, 10.0)
+	
+	if position.y > 800:
+		position = initialPos;
+		linear_velocity = Vector2.ZERO;
 		
 func _input(event):
-	if event.is_action_pressed("mouse_button") and !dragging:
-		dragging = true
-		dragStart = get_global_mouse_position()
-	if event.is_action_released("mouse_button") and dragging:
-		dragging = false
-		var _dragEnd = get_global_mouse_position()
-		var dir = dragStart - _dragEnd
-		apply_impulse(dir * 5)
+	pass
+	#if event.is_action_pressed("mouse_button") and !dragging:
+		#dragging = true
+		#dragStart = get_global_mouse_position()
+	#if event.is_action_released("mouse_button") and dragging:
+		#dragging = false
+		#var _dragEnd = get_global_mouse_position()
+		#var dir = dragStart - _dragEnd
+		#apply_impulse(dir * 5)
 
